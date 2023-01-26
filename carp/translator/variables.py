746251f19d8da04645478f97953e0570602297d0
niqzart <qwert45hi@yandex.ru>
2023-01-26 03:07:02 +0300

    feat: helper elements: variables & comparators

---
+++ carp/translator/variables.py
@@ -0,0 +1,36 @@
+import re
+from typing import Pattern
+
+from pydantic import BaseModel
+
+from common.errors import TranslationError
+
+
+class VarDef(BaseModel):
+    name: str
+    location: int
+
+
+class VariableIndex:
+    VARIABLE_REGEX: Pattern[str] = re.compile(r"[a-z_][a-z_0-9]*")
+
+    def __init__(self) -> None:
+        self.variables: dict[str, int] = {}
+        self.next_location: int = 16
+
+    def check_name(self, name: str) -> bool:
+        return re.fullmatch(self.VARIABLE_REGEX, name) is not None
+
+    def register(self, name: str) -> int:
+        if not re.fullmatch(self.VARIABLE_REGEX, name):
+            raise TranslationError(f"Unsupported variable name: '{name}'")
+        if name not in self.variables:
+            self.variables[name] = self.next_location
+            self.next_location += 1
+        return self.variables[name]
+
+    def read(self, name: str) -> VarDef:
+        location = self.variables.get(name)
+        if location is None:
+            raise TranslationError(f"Variable '{name}' is not defined")
+        return VarDef(name=name, location=location)
