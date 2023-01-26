9a0960941ae967fb800e4131d4584a0a58cb5b7f
niqzart <qwert45hi@yandex.ru>
2023-01-26 13:30:18 +0300

    feat: start on executor with constants & alu

--- carp/common/constants.py
+++ carp/common/constants.py
@@ -2,3 +2,8 @@ INPUT_ADDRESS: int = 1
 """A memory address, mapped to the input device"""
 OUTPUT_ADDRESS: int = 3
 """A memory address, mapped to the output device"""
+
+WORD_LENGTH: int = 32
+WORD_MAIN: int = 2 ** (WORD_LENGTH - 1)
+WORD_MAX_VALUE: int = WORD_MAIN - 1
+WORD_MIN_VALUE: int = -WORD_MAIN
