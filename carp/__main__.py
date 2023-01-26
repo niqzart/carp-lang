6bbdfa553df46bfd2af5d4af06b98265c3e394b4
niqzart <qwert45hi@yandex.ru>
2023-01-26 03:07:41 +0300

    feat: full translation command & error handling

--- carp/__main__.py
+++ carp/__main__.py
@@ -2,17 +2,10 @@ import json
 from pathlib import Path
 from typing import Optional
 
-from pydantic import parse_raw_as
 from typer import Typer, FileText, Argument
 
 from common.errors import TranslationError
-from common.operations import (
-    BinaryOperation,
-    StackOperation,
-    JumpOperation,
-    MemoryOperation,
-    Operation,
-)
+from common.operations import Operation
 from translator.parser import ParserError
 from translator.reader import Reader
 from translator.translator import Translator
@@ -31,6 +24,35 @@ def translate(
         output_path = input_path + ".curp"
 
     code = input_file.read()
+    try:
+        reader = Reader(code)
+        print("Parsing successful")
+        if save_parsed:
+            parsed = [symbol.dict() for symbol in reader.symbols]
+            with open(input_path + ".cpar", "w") as f:
+                json.dump(parsed, f, indent=2)
+            print("Parsing result saved")
+    except ParserError as e:
+        print(str(e))
+
+    try:
+        translator: Translator = Translator(reader=reader)
+        translator.parse_blocks()
+        compiled = [operation.dict() for operation in translator.result]
+
+        with open(output_path, "w") as f:
+            json.dump(compiled, f, indent=2)
+        print("Compilation successful")
+        print(f"Result has been saved to {output_path}")
+    except TranslationError as e:
+        translator.reader.back()
+        symbol = translator.reader.current_or_closing()
+        print(
+            f"Translation error occurred at "
+            f"{symbol.line}:{symbol.char} "
+            f"({symbol.text}): {e}"
+        )
+        raise e
 
 
 @app.command()
