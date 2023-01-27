7ed7083bb764b52bf1bad336b8134b8a653a2881
niqzart <qwert45hi@yandex.ru>
2023-01-27 19:13:18 +0300

    feat(test): the integrational tests

---
+++ carp/tests/integrational/test_programs.py
@@ -0,0 +1,36 @@
+import subprocess
+from pathlib import Path
+
+import pytest
+
+
+EXAMPLE_FOLDER: Path = Path("examples")
+
+
+def check_output(*args: str) -> str:
+    return subprocess.check_output(
+        ["python", "carp", *args],
+        cwd="./.."
+    ).decode("utf-8").strip().replace("\r\n", "\n")
+
+
+@pytest.mark.parametrize(
+    ("program_name", "file_in", "expected"),
+    [
+        pytest.param("hello", False, "Hello World", id="hello"),
+        pytest.param("cat", True, None, id="cat"),
+        pytest.param("prob2", False, "", id="prob2"),
+    ],
+)
+def test_one(program_name: str, file_in: bool, expected: str | None):
+    check_output("translate", str(EXAMPLE_FOLDER / f"{program_name}.carp"))
+
+    execute_command = ["execute", str(EXAMPLE_FOLDER / f"{program_name}.curp")]
+    if file_in:
+        execute_command.append(str(EXAMPLE_FOLDER / f"{program_name}.in.txt"))
+
+    if expected is None:
+        with open(".." / EXAMPLE_FOLDER / f"{program_name}.in.txt") as f:
+            expected = f.read() + "\0"
+
+    assert check_output(*execute_command) == expected
